e77936f90378acb9665f012efd845219d8fe7163
robert koopedi <koopedi05@gmail.com>
2024-09-06 18:52:40 +0200

    blackjack game development

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard;
 
-let firstCard = 6
-let secondCard = 9
+let player = {
+    name: "Per",
+    chips: 200
+};
 
-let sum = firstCard + secondCard
+let cards = [firstCard, secondCard];
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-console.log(sum)
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
+}
+
+function startGame() {
+    cards = [getRandomCard(), getRandomCard()];
+    sum = cards[0] + cards[1];
+    hasBlackJack = false;
+    isAlive = true;
+    renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards.join(' ');
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum < 21) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    if (isAlive && !hasBlackJack) {
+        let card = getRandomCard();
+        cards.push(card);
+        sum += card;
+        renderGame();
+    }
+}
+
+startGame();
